7d94419b1817e30b0eee2457b2dabbddb4b36da3
yroquain <yroquain@gmail.com>
2015-11-23 20:49:36 -0500

    Modif circuit 8 + ajout zones dans premier circuit

---
+++ Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts
+{
+    public class CheckPoint
+    {
+        public Vector3 p1;
+        public Vector3 p2;
+
+        public CheckPoint(float p1x, float p1y, float p1z, float p2x, float p2y, float p2z)
+        {
+            p1 = new Vector3(p1x, p1y, p1z);
+            p2 = new Vector3(p2x, p2y, p2z);
+        }
+    }
+}
---
+++ Assets/Scripts/ReboundScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReboundScript : MonoBehaviour {
+
+    public float height;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        Rigidbody rigidBody = GetComponent<Rigidbody>();
+        Vector3 heightVector = new Vector3(rigidBody.position.x, height, rigidBody.position.z);
+        rigidBody.position = heightVector;
+
+
+	}
+}
